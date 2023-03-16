c244a9f7158c1211d88d76d7148bf8854a4c23ac
Nicolas Fernandez <nico.06.89crc@gmail.com>
2023-03-16 16:57:40 +1100

    About section improvement.

--- src/pages/sections/About/About.js
+++ src/pages/sections/About/About.js
@@ -1,11 +1,11 @@
 import { Grid, Stack, Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 
 const KnowloedgeContainer = ({source, knowledgeName}) => {
   return (
     <Grid item sx={{ padding: '0 !important', paddingRight: '2rem !important', paddingBottom: '2rem !important' }}>
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <Stack sx={{height: '50px', width: '50px', "img": { width: "100%" }}}>
+      <Stack direction="row" alignItems="center" spacing={3}>
+        <Stack sx={{height: '75px', width: '75px', "img": { width: "100%" }}}>
           <img src={source} alt={knowledgeName}/>
         </Stack>
         <Typography variant="body1">{knowledgeName}</Typography>
@@ -18,14 +18,18 @@ const About = () => {
   const { t } = useTranslation();
   
   return (
-    <Grid container spacing={6}>
+    <Grid container spacing={6} paddingY={8}>
       <Grid item>
         <Stack spacing={5}>
           <Typography variant="h3" alignSelf="center">{t('about.title')}</Typography>
-          <Stack gap={2}>
-            <Typography variant="body2" fontSize="1.15rem">{t('about.description_1')}</Typography>
-            <Typography variant="body2" fontSize="1.15rem">{t('about.description_2')}</Typography>
-            <Typography variant="body2" fontSize="1.15rem">{t('about.description_3')}</Typography>
+          <Stack spacing={5}>
+            <Trans i18nKey="about.description">
+              <Typography fontWeight="300" variant="h6">As a Full-Stack Web Developer with over 2 years of experience in developing Web Applications, I have acquired a strong foundation in Javascript (React, NodeJs, Express, MongoDB), CSS, HTML, GIT, AWS, Docker, and Ruby on Rails.</Typography>
+              <Typography fontWeight="300" variant="h6">I have extensive experience in developing complex web applications from the ground up. I have worked on various projects where I have designed and implemented RESTful APIs, integrated third-party services, and created responsive front-end interfaces using React and Ruby on Rails with Bootstrap, Jquery, Postgress.</Typography>
+              <Typography fontWeight="300" variant="h6">In addition to my technical skills, I am also well-versed in the Agile development methodology and have experience working in fast-paced environments where I had to prioritize and deliver features within tight deadlines.</Typography>
+              <Typography fontWeight="300" variant="h6">My proficiency in Ruby on Rails has also enabled me to build efficient web applications with clean code and well-defined architecture. I have a deep understanding of MVC architecture and have implemented it in my projects to ensure the separation of concerns and maintainability of code.</Typography>
+              <Typography fontWeight="300" variant="h6">I believe that my excellent communication skills and team-oriented mindset will make me an asset to your team. I am passionate about learning new technologies and working with a diverse group of people to deliver high-quality products.</Typography>
+            </Trans>
           </Stack>
         </Stack>
       </Grid>
