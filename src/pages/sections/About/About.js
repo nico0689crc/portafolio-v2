93e1439a78457ea5ca85896f6c42fa769fbcfe44
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-01-25 00:59:31 +0800

    Update About.js

--- src/pages/sections/About/About.js
+++ src/pages/sections/About/About.js
@@ -34,7 +34,57 @@ const About = () => {
       <Stack spacing={5}>
         <Typography variant="h4" alignSelf="center">{t('about.title')}</Typography>
         <Stack spacing={5}>
-          <div><p>Your extensive experience as a Full-Stack Web Developer showcases a well-rounded skill set. Let's break down your key strengths and achievements:</p><ol><li><p><strong>Technical Skills:</strong></p><ul><li>Proficiency in JavaScript (React, Node.js, Express) and Ruby on Rails.</li><li>Strong front-end development skills with HTML, CSS, and responsive design using Bootstrap and jQuery.</li><li>Backend development expertise with Node.js, Express, MongoDB, and Ruby on Rails.</li><li>Experience in designing and implementing RESTful APIs.</li><li>Version control using Git.</li><li>Cloud computing with AWS.</li><li>Containerization with Docker.</li></ul></li><li><p><strong>Project Experience:</strong></p><ul><li>Developing complex web applications from scratch.</li><li>Designing and implementing RESTful APIs for seamless communication between frontend and backend.</li><li>Integration of third-party services to enhance application functionality.</li><li>Creation of responsive and user-friendly front-end interfaces using React and Ruby on Rails with Bootstrap and jQuery.</li><li>Database management using PostgreSQL.</li></ul></li><li><p><strong>Agile Development:</strong></p><ul><li>Familiarity and experience with Agile development methodologies.</li><li>Thriving in fast-paced environments and meeting tight deadlines.</li><li>Effective prioritization and delivery of features within project timelines.</li></ul></li><li><p><strong>Ruby on Rails Proficiency:</strong></p><ul><li>Deep understanding of MVC architecture.</li><li>Building efficient web applications with clean code and a well-defined architecture.</li><li>Ensuring separation of concerns and code maintainability.</li></ul></li><li><p><strong>Communication and Teamwork:</strong></p><ul><li>Excellent communication skills.</li><li>A team-oriented mindset, emphasizing collaboration and shared success.</li><li>Passionate about working with diverse teams to deliver high-quality products.</li></ul></li><li><p><strong>Adaptability and Continuous Learning:</strong></p><ul><li>A passion for learning new technologies.</li><li>Adaptability to evolving industry trends and tools.</li></ul></li></ol><p>With such a strong background, you bring a valuable mix of technical expertise, project management skills, and a collaborative approach to your work. Your commitment to continuous learning and ability to thrive in fast-paced environments make you a promising candidate for contributing to and enhancing any development team.</p></div>
+          <div>
+            <p>I am a dedicated Full Stack Web Developer who prioritizes collaboration within the team and is passionate about serving the needs of our clients. </p>
+            <p>Currently, I am actively learning and honing my skills in high-demand languages like Javascript, and its associated stack, including MongoDB, ExpressJs, ReactJs, NodeJs, and Ruby on Rails.</p>
+            <ol>
+              <li>
+                <p><strong>Technical Skills:</strong></p>
+                <ul>
+                  <li>Proficiency in JavaScript (React, NextJS, Node.js, Express).</li>
+                  <li>Strong front-end development skills with HTML, CSS, and responsive designing.</li>
+                  <li>Backend development expertise with Node.js, Express, MongoDB, and Ruby on Rails.</li>
+                  <li>Experience in designing and implementing RESTful APIs.</li>
+                  <li>Version control using Git.</li>
+                  <li>Cloud computing with AWS.</li>
+                  <li>Containerization with Docker.</li>
+                </ul>
+              </li>
+              <li>
+                <p><strong>Project Experience:</strong></p>
+                <ul>
+                  <li>Designing and implementing RESTful APIs for seamless communication between frontend and backend.</li>
+                  <li>Integration of third-party services to enhance application functionality.</li>
+                  <li>Creation of responsive and user-friendly front-end interfaces using React, NextJS, MUI, Bootstrap and jQuery.</li>
+                  <li>Database management using PostgreSQL, MySql and MongoDB</li>
+                </ul>
+              </li>
+              <li>
+                <p><strong>Agile Development:</strong></p>
+                <ul>
+                  <li>Familiarity and experience with Agile development methodologies.</li>
+                  <li>Thriving in fast-paced environments and meeting tight deadlines.</li>
+                  <li>Effective prioritization and delivery of features within project timelines.</li>
+                </ul>
+              </li>
+              <li>
+                <p><strong>Communication and Teamwork:</strong></p>
+                <ul>
+                  <li>Excellent communication skills.</li>
+                  <li>A team-oriented mindset, emphasizing collaboration and shared success.</li>
+                  <li>Passionate about working with diverse teams to deliver high-quality products.</li>
+                </ul>
+              </li>
+              <li>
+                <p><strong>Adaptability and Continuous Learning:</strong></p>
+                <ul>
+                  <li>A passion for learning new technologies.</li>
+                  <li>Adaptability to evolving industry trends and tools.</li>
+                </ul>
+              </li>
+            </ol>
+            <p>With such a strong background, I can bring a valuable mix of technical expertise, and a collaborative approach to work. My commitment to continuous learning and ability to thrive in fast-paced environments make to me a promising candidate for contributing to and enhancing any development team.</p>
+          </div>
         </Stack>
       </Stack>
       <Stack spacing={5}>
