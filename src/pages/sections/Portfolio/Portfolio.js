24f87637e4c9270d9a22a639468a32fbb647e213
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-01-26 14:06:37 +0800

    Updating portfolio and addint i18n

--- src/pages/sections/Portfolio/Portfolio.js
+++ src/pages/sections/Portfolio/Portfolio.js
@@ -4,35 +4,28 @@ import IconButtonSocialMedia from "components/ui/IconButtonSocialMedia/IconButto
 import Iconify from "components/ui/Iconify";
 import { useTranslation } from "react-i18next";
 import PortafolioImage from "assets/images/portafolio.png";
+import KanbanImage from "assets/images/kanban.png";
 import { motion } from "framer-motion";
 
 const projects = [{
-  "title": "Kanban dashboard (Work in Progress)",
-  "description": "Amet sint esse ad cupidatat eu. Quis adipisicing labore duis amet cillum irure. Elit deserunt labore nisi ut. Et consectetur exercitation quis nisi id culpa laboris fugiat do minim esse elit cupidatat sint.",
+  "title": "portfolio.projects.kanban.title",
+  "description": "portfolio.projects.kanban.description",
   "tags": ["ReactJs", "Docker", "NextJS", "MUI", "Redux", "Typescript", "NodeJS", "ExpressJs", "Mysql", "Sequelize", "Api", "Fullstack", "Sendgrid"],
   "github": "https://github.com/nico0689crc/kanban",
   "website": "https://kanban.nicolasarielfernandez.tech",
-  "image": null
+  "image": KanbanImage
 },{
-  "title": "Portfolio",
-  "description": "Est ad duis incididunt incididunt eu nostrud irure sit. Non nostrud id est cillum adipisicing quis ut minim duis sunt sit pariatur. Aute dolor Lorem culpa Lorem mollit dolore ex deserunt dolor do enim. Qui et veniam dolore do aliqua cillum ullamco adipisicing veniam.",
+  "title": "portfolio.projects.portfolio.title",
+  "description": "portfolio.projects.portfolio.description",
   "tags": ["ReactJs", "MUI"],
   "github": "https://github.com/nico0689crc/portafolio-v2",
   "website": "https://nicolasarielfernandez.tech",
   "image": PortafolioImage
-},{
-  "title": "Shop Challenge",
-  "description": "Laboris adipisicing sit aliquip aliquip veniam ad ipsum sint. Eu commodo enim officia nulla consequat sunt elit. Tempor nisi id ullamco ea dolor ipsum cupidatat aliquip reprehenderit. Ad proident enim deserunt qui ipsum ea duis non non est do ullamco exercitation. Tempor laboris dolor incididunt fugiat dolor. Ipsum voluptate anim irure Lorem elit occaecat exercitation occaecat elit id sunt anim sunt. Ullamco aute reprehenderit culpa excepteur aliqua eu ullamco commodo fugiat ipsum non nostrud ad aute.",
-  "tags": ["ReactJs", "Redux", "NodeJS", "Mysql", "Api", "Fullstack"],
-  "github": [
-    { title: "GitHub - Frontend", url: "https://github.com/nico0689crc/itcrowd-challenge-frontend"},
-    { title: "GitHub - Backend", url: "https://github.com/nico0689crc/itcrowd-challenge"}
-  ],
-  "website": "#",
-  "image": null
 }];
 
 const ProjectCard = ({project}) => {
+  const { t } = useTranslation();
+
   return (
     <Grid item xs={12} sm={6} md={6} xl={4}>
       <Box
@@ -54,7 +47,7 @@ const ProjectCard = ({project}) => {
               />
             </Grid>
             <Grid item xs={12} sx={{ borderBottom: theme => `1px dashed ${theme.palette.primary.main}` }}>
-              <Stack p={3} spacing={1}>
+              <Stack p={3} spacing={2}>
                 <Link href={!Array.isArray(project.website) ? project.website : "#" } target="_blank">
                   <Typography variant="subtitle1" 
                   sx={{ 
@@ -63,12 +56,12 @@ const ProjectCard = ({project}) => {
                       gap: "0.5rem", 
                       color: (theme) => theme.palette.mode === "dark" ? theme.palette.primary.main : theme.palette.text.primary 
                     }}>
-                      {project.title}
+                      {t(project.title)}
                     <Iconify icon="fa:external-link" sx={{ width: 15 }}/>
                   </Typography>
                   
                 </Link>
-                <Typography variant="caption">{project.description}</Typography>
+                <Typography variant="body2">{t(project.description)}</Typography>
                 <Grid container gap={1.5}>
                   {project.tags.map((tag, index) => (
                     <Grid key={index} item>
